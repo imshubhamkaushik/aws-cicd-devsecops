04a6aebb8860c96710bc4036d07ae68e03d9d054
Shubham <shubhamkaushik91@gmail.com>
2026-04-16 14:48:51 +0530

    Updated Files

--- terraform/platform-infra/env/dev/backend.tf
+++ terraform/platform-infra/env/dev/backend.tf
@@ -33,6 +33,7 @@ provider "aws" {
 provider "kubernetes" {
   host                   = local.cluster_endpoint
   cluster_ca_certificate = base64decode(local.cluster_certificate)
+  
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
@@ -44,6 +45,7 @@ provider "helm" {
   kubernetes = {
     host                   = local.cluster_endpoint
     cluster_ca_certificate = base64decode(local.cluster_certificate)
+    
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
