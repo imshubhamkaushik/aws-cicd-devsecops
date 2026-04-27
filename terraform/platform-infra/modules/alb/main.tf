f877681e42dae89273ccfcd053a4bf998a01a6b5
Shubham <shubhamkaushik91@gmail.com>
2026-04-27 19:40:16 +0530

    Updated Files

--- terraform/platform-infra/modules/alb/main.tf
+++ terraform/platform-infra/modules/alb/main.tf
@@ -74,7 +74,7 @@ resource "helm_release" "alb_controller" {
   ]
 
   depends_on = [
-    kubernetes_service_account.alb,
+    kubernetes_service_account_v1.alb,
     aws_iam_role_policy_attachment.alb
   ]
 }
