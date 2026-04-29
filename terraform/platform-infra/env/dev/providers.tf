21d1cebef99a175011880cb01ebcc2fad1b29c43
Shubham <shubhamkaushik91@gmail.com>
2026-04-29 20:23:15 +0530

    Updated Files

--- terraform/platform-infra/env/dev/providers.tf
+++ terraform/platform-infra/env/dev/providers.tf
@@ -21,7 +21,7 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  alias = "after_eks"
+  alias                  = "after_eks"
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
 
--- terraform/platform-infra/modules/eks/main.tf
+++ terraform/platform-infra/modules/eks/main.tf
@@ -47,7 +47,7 @@ resource "aws_eks_cluster" "cluster" {
     endpoint_private_access = true
     # DEV NOTE: public access is on so you can run kubectl from your laptop.
     # For production set this to false and access only from within the VPC.
-    endpoint_public_access = false              # for production, set to false
+    endpoint_public_access = false # for production, set to false
     # public_access_cidrs    = [local.my_ip_cidr] # auto-locked to your IP at apply time | use this if endpoint_public_access = true
   }
 
