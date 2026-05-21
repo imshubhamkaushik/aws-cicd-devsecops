7269423cd87b5e14009c6f0dd2589762d116a68f
Shubham <shubhamkaushik91@gmail.com>
2026-05-22 02:30:09 +0530

    Updated Files

--- terraform/platform-infra/modules/eks/main.tf
+++ terraform/platform-infra/modules/eks/main.tf
@@ -5,6 +5,8 @@ data "http" "my_ip" {
   url = "https://checkip.amazonaws.com"
 }
 
+data "aws_caller_identity" "current" {}
+
 locals {
   my_ip_cidr = "${chomp(data.http.my_ip.response_body)}/32"
 }
